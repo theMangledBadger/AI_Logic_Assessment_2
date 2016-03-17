721f63da1c4465b2f577d64659b409589e005594
Emmett <daly.emmett@gmail.com>
2016-03-17 16:17:17 +0000

    Adding updated file as worked on by Sam, Alan and Emmett on Sam's machine

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import csv
 # Adding numpy dependency
-import numpy
+import numpy as np
+from sklearn import svm
 
 # Import file
 training_file_data = "training_data.csv"
+testing_file_data = "testing_data.csv"
 
-def training_data_file_reader(file_path):
+classifier = svm.SVC()
+
+
+def file_reader(file_path):
     # The neural net module we have requires two inputs:
     # a ist of training data lists as input on
     # a list of expected outputs, ie the 'Type' as identified
 
-    # We have two lists we need to store:
-    training_list = []
-    glass_type_list = []
+    data=[]
 
     with open(file_path) as csv_file:
         reader = csv.DictReader(csv_file)
 
         for row in reader:
-            # Each item identified as 'Type', we append the value to the list
-            glass_type_list.append([int(row['Type'])])
             # Each item identified as not 'id' or not 'Type', we append to the inputs list
             temp = [float(row['RI']),
                     float(row['Na']),
@@ -30,13 +31,51 @@ def training_data_file_reader(file_path):
                     float(row['Ca']),
                     float(row['Ba']),
                     float(row['Fe'])]
-            training_list.append(temp)
+            data.append(temp)
 
-    # At the moment, we are just printing the data to the screen in two lists:
-    print(training_list)
-    print(glass_type_list)
-    return training_list
+    return data
+
+def get_expected_outputs(file_path):
+    
+    data=[]
+
+    with open(file_path) as csv_file:
+        reader = csv.DictReader(csv_file)
+
+        for row in reader:
+            data.append(int(row['Type']))
+
+    return data
+
+def main():
+    # get training/testing sets and expected outputs from file
+    training_set = file_reader(training_file_data)
+    testing_set = file_reader(testing_file_data)
+    training_expected_outputs = get_expected_outputs(training_file_data)
+    testing_expected_outputs = get_expected_outputs(testing_file_data)
+    actual_output = []
+
+    # create a numpy array for training set/expected outputs
+    X = np.array(training_set)
+    Y = np.array(training_expected_outputs)
+
+    # run fitness function on training set array/expected output array
+    classifier.fit(X,Y)
+
+    # for each record in testing set, run prediction function and append output to expected output list
+    for record in testing_set:
+        test = np.array([record])
+        output = classifier.predict(test)
+        actual_output.append(output[0])
+
+    # print output
+    print('predicted outputs:')
+    print(testing_expected_outputs)
+    print('actual outputs:')
+    print(actual_output)
+
+    pass
 
 
 if __name__ == '__main__':
-    training_data_file_reader(training_file_data)
+    main()
--- neural_net_numpy_example.py
+++ neural_net_numpy_example.py
@@ -1,5 +1,4 @@
-from numpy import numpy as np
-
+import numpy as np
 
 # sigmoid function
 def nonlin(x, deriv=False):
@@ -9,22 +8,28 @@ def nonlin(x, deriv=False):
 
 
 # input dataset
-X = np.array([[0, 0, 1],
-              [0, 1, 1],
-              [1, 0, 1],
-              [1, 1, 1]])
+X = np.array([[1.51617, 14.95, 0.0, 2.27, 73.3, 0.0, 8.71, 0.67, 0.0], 
+    [1.51732, 14.95, 0.0, 1.8, 72.99, 0.0, 8.61, 1.55, 0.0], 
+    [1.51645, 14.94, 0.0, 1.87, 73.11, 0.0, 8.67, 1.38, 0.0], 
+    [1.51831, 14.39, 0.0, 1.82, 72.86, 1.41, 6.47, 2.88, 0.0], 
+    [1.5164, 14.37, 0.0, 2.74, 72.85, 0.0, 9.45, 0.54, 0.0], 
+    [1.51623, 14.14, 0.0, 2.88, 72.61, 0.08, 9.18, 1.06, 0.0], 
+    [1.51685, 14.92, 0.0, 1.99, 73.06, 0.0, 8.4, 1.59, 0.0], 
+    [1.52065, 14.36, 0.0, 2.02, 73.42, 0.0, 8.44, 1.64, 0.0], 
+    [1.51651, 14.38, 0.0, 1.94, 73.61, 0.0, 8.48, 1.57, 0.0], 
+    [1.51711, 14.23, 0.0, 2.08, 73.36, 0.0, 8.62, 1.67, 0.0]])
 
 # output dataset
-y = np.array([[0, 0, 1, 1]]).T
+y = np.array([[0, 0, 1, 1, 0, 0, 1, 0, 1, 0]]).T
 
 # seed random numbers to make calculation
 # deterministic (just a good practice)
 np.random.seed(1)
 
 # initialize weights randomly with mean 0
-syn0 = 2 * np.random.random((3, 1)) - 1
+syn0 = 2 * np.random.random((9, 1)) - 1
 
-for iter in xrange(10000):
+for iter in range(10000):
     # forward propagation
     l0 = X
     l1 = nonlin(np.dot(l0, syn0))
